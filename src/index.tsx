d4ea9c51c458103fbcaff1006d5399ac14c1ba92
Lucas Eduardo <lucased78@hotmail.com>
2020-04-11 20:48:57 -0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render }  from 'react-dom';
+
+const App = () => <div>sajdasoidjsaoidjas</div>
+
+render(<App />, document.getElementById('root'));
---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.tsx'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|tsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        }]
+      },
+      {
+        test: /\.css$/,
+        exclude: /src/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|ttf)/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.tsx', '.ts']
+  }
+}
