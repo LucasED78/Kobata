b0bc2f5c5a393de03f50c740c1cc40ef4aef59a6
Lucas Eduardo <lucased78@hotmail.com>
2020-04-11 22:59:08 -0300

    [ADDED] Radio component

---
+++ src/components/RadioWithLabel/RadioWithLabel.tsx
@@ -0,0 +1,37 @@
+import React, { ChangeEvent } from 'react';
+import classes from './RadioWithLabel.css';
+import { v4 as uuidv4 } from 'uuid';
+
+export interface RadioWithLabelProps {
+  label: string;
+  value: string;
+  id?: string;
+  name: string;
+  onChange(event: ChangeEvent<HTMLInputElement>): void;
+}
+
+const RadioWithLabel = (props: RadioWithLabelProps) => {
+  const id = props.label ?? uuidv4();
+
+  return (
+    <>
+      <label htmlFor={id}>
+        <input 
+          type="radio" 
+          id={id} 
+          name={props.name}
+          onChange={props.onChange}
+          value={props.value} />
+        
+        <span className={classes.Radio}>
+          <div className={classes['Radio--checked']}></div>
+        </span>
+        
+        <span className={classes.Radio__label}>esse e o label</span>
+      </label>
+      
+    </>
+  )
+}
+
+export default RadioWithLabel;
